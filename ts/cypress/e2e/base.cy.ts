fc0835c8e87f1ffed853ce8e53c36cef59ebd42b
Natzaum <natacezerbordignon@gmail.com>
2023-10-13 02:28:28 -0300

    tests

--- ts/cypress/e2e/base.cy.ts
+++ ts/cypress/e2e/base.cy.ts
@@ -18,23 +18,24 @@ describe('checa elementos básicos', () => {
   });
 
   it('expandir tópico funciona', () => {
-    cy.get('.answered-topic .ops-topic-subject').click();
+    cy.get('.answered-topic').eq(0).click();
+    cy.get('.ops-topic-subject').eq(0).click();
     cy.get('.comments-container').should('exist');
     cy.compareSnapshot('Trabalho - Card de topico expandido');
   });
 
   it('clicar em `criar tópico` exibe o formulário', () => {
-    cy.get('.btn-create-topic').click();
+    cy.get('.btn-create-topic').eq(0).click();
     cy.get('button').contains('Enviar')
-    cy.get('form').contains('Assunto');
-    cy.get('form').contains('Conteúdo');
-    cy.get('input.subject').invoke('attr', 'placeholder').should('contain', 'Defina um tópico sucinto para notificar os autores...');
+    cy.get('.assunto').contains('Assunto');
+    cy.get('body').contains('Conteúdo');
+    cy.get('body').contains('Defina um tópico sucinto para notificar os autores...');
     cy.compareSnapshot('Trabalho - Criando novo topico');
   });
 
   it('enviar o formulário exibe mensagem de sucesso', () => {
-    cy.get('.btn-create-topic').click();
-    cy.get('button').click();
+    cy.get('.btn-create-topic').eq(0).click();
+    cy.get('button').eq(0).click();
     cy.get('body').contains('Aguardando feedback dos autores');
     cy.compareSnapshot('Trabalho - Topico enviado');
   });
